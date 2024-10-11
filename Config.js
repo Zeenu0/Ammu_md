ff6b55ee850837ba4b31820d1458e3322508ec6c
Zeenu0 <mhdamaan520@gmail.com>
2024-10-11 22:59:15 +0530

    Update Config.js

--- Config.js
+++ Config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "TnhnTYDD#Jg6gkDXNp-4CM5yo-Zm-rUvlhZ6wAMOVNaMnIJkD1To",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
-AUTO_REPLY: process.env.AUTO_REPLY || "false",
+AUTO_REPLY: process.env.AUTO_REPLY || "true",
 };
