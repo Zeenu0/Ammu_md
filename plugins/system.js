334ecc81e698b31c173dd73a1c33a88d1ee63f83
Zeenu0 <mhdamaan520@gmail.com>
2024-10-11 19:54:03 +0530

    Create system.js

---
+++ plugins/system.js
@@ -0,0 +1,63 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const os = require("os")
+const {runtime} = require('../lib/functions')
+
+cmd({
+    pattern: "system",
+    desc: "Check bot online or no.",
+    category: "main",
+    react: "🖥️",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+    let status = `
+───────────────────
+_*⚙️ a m m u Ｓｙｓｔｅｍ  Ｉｎｆｏ ⚙️*_
+───────────────────
+
+┌────────────────
+│❖ *ᴜᴘᴛɪᴍᴇ :* _${runtime(process.uptime())}_
+│❖ *ʀᴀᴍ ᴜꜱᴀɢᴇ :*  _${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB_
+│❖ *ʜᴏꜱᴛ ɴᴀᴍᴇ :* _${os.hostname()}_
+│❖ *ᴏᴡɴᴇʀ :* _Amaan Developer_
+└────────────────
+
+> ᴄʀᴇᴀᴛᴇᴅ ʙʏ Amaan Developer 
+`
+
+    return reply(`${status}`)
+
+
+
+}catch(e){
+    console.log(e)
+    reply(`${e}`)
+    }
+    })
+
+
+//__________ping______
+
+cmd({
+    pattern: "ping",
+    desc: "Check bot online or no.",
+    category: "main",
+    react: "🚀",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+    const startTime = Date.now()
+        const message = await conn.sendMessage(from, { text: '*pong...*' })
+        const endTime = Date.now()
+        const ping = endTime - startTime
+        await conn.sendMessage(from, { text: `_*📍ammu Speed : ${ping}ms*_` }, { quoted: mek })
+    } catch (e) {
+        console.log(e)
+        reply(`${e}`)
+    }
+})
+
